8ee8e09f0dba26636c227e149dad0c35eee2bb07
wkacsur <wkacsur@umich.edu>
2017-11-08 23:24:55 -0500

    add main

--- test_rpn.py
+++ test_rpn.py
@@ -6,12 +6,15 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2, result)
+        print('add')
     def test_subtract(self):
         result = rpn.calculate("5 3 -")
         self.assertEqual(2, result)
+        print('sub')
     def test_multiply(self):
         result = rpn.calculate("5 3 *")
         self.assertEqual(15, result)
+        print('mult')
     def test_divide(self):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
@@ -19,3 +22,5 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate("2 3 ^")
         self.assertEqual(8, result)
     
+if __name__ == '__main__':
+    unittest.main()
